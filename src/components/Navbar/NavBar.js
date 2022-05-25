31864503048fb2b9b290168803b483a4ee42d473
sanjay-sp <sanjayforofficial@gmail.com>
2022-05-25 20:24:29 +0530

    first commit

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './NavBar.css'
+
+const NavBar = () => {
+    return <div>
+        <nav>
+            <div className='logo'>Pokedex</div>
+        </nav>
+    </div>
+}
+
+export default NavBar;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './SearchBar.css'
+
+const SearchBar = () => {
+    return <div>
+        <input type="textbox" className="searchbar" placeholder="Enter Search Term..."/>
+    </div>
+}
+
+export default SearchBar;
